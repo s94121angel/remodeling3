dbe50e81e0c3364d242289c9571c93bf08f2e1cd
s94121angel <s94121angel@yahoo.com.tw>
2022-07-31 13:48:18 +0800

    20220731_1:50
    Signed-off-by: s94121angel <s94121angel@yahoo.com.tw>

--- train.py
+++ train.py
@@ -20,7 +20,7 @@ from datetime import datetime
 now = datetime.now()
 current_time = now.strftime("%H:%M:%S")
 print("Current Time =", current_time)
-print("Angel is testing the CI/CD")
+#print("Angel is testing the CI/CD")
 
 initial_count=0  
 
